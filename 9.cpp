ffe4f82188f524ae5abed75b29ee53d98314b91c
oyly16 <675308060@qq.com>
2018-12-20 16:02:36 +0800

    求出以i为左右边界的最大面积

--- 9.cpp
+++ 9.cpp
@@ -1,22 +1,91 @@
 #include<stdio.h>
 
-int N,K;
-int h[12000];
-int maxleft[12000], maxright[12000];//从左开始和从右开始，n块土地的最大面积
-
-void getMaxLeft(int n)
+typedef struct
 {
+	int height;//这块地的高度
+	bool blocked=0;//这块地是否能继续往右/左扩展
+	int length=1;//以这块地为短板能扩展的长度
+} Rect;
 
+int N,K;
+Rect r[12000];
+int maxleft[12000], maxright[12000];//以第n块地为左右边界的最大面积
+
+int max(int a, int b, int c)
+{
+	if (a > b && a > c) return a;
+	if (b > c) return b;
+	return c;
+}
+
+int max(int a, int b)
+{
+	if (a > b) return a;
+    return b;
 }
 
 void getMaxRight(int n)
 {
+	int tempmax = 0, s;
+	for (int i = 0; i < n; i++)
+	{
+		if (r[i].blocked == 1) continue;
+		if (r[i].height > r[n].height) r[i].blocked = 1;
+		else
+		{
+			r[i].length++;
+			s = r[i].height*r[i].length;
+			if (s > tempmax) tempmax = s;
+		}
+	}//得到tempmax
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (r[i].height < r[n].height) break;
+		r[n].length++;
+	}
+	maxright[n] = max(tempmax, r[n].height*r[n].length);
+}
 
+void getMaxLeft(int n)
+{
+	int tempmax = 0, s;
+	for (int i = N - 1; i > n; i--)
+	{
+		if (r[i].blocked == 1) continue;
+		if (r[i].height > r[n].height) r[i].blocked = 1;
+		else
+		{
+			r[i].length++;
+			s = r[i].height*r[i].length;
+			if (s > tempmax) tempmax = s;
+		}
+	}//得到tempmax
+	for (int i = n + 1; i < N; i++)
+	{
+		if (r[i].height < r[n].height) break;
+		r[n].length++;
+	}
+	maxleft[n] = max(tempmax, r[n].height*r[n].length);
 }
 
 int main()
 {
 	scanf_s("%d%d", &N, &K);
-	for (int i = 0; i < N; i++) scanf_s("%d", &h[i]);
-	maxleft[0] = h[0]; maxright[0] = h[N - 1];
+	for (int i = 0; i < N; i++) scanf_s("%d", &r[i].height);
+	maxright[0] = r[0].height; maxleft[0] = r[N - 1].height;
+	for (int i = 1; i < N; i++) getMaxRight(i);
+	for (int i = N - 1; i >= 0; i--)
+	{
+		r[i].blocked = 0; r[i].length = 1;
+		getMaxLeft(i);
+	}
+	/*for (int i = 0; i < N; i++)
+	{
+		printf("%d %d\n", maxleft[i], maxright[i]);
+	}*/
+	int ans = 0, temp;
+	for (int i = 0; i < N - K - 1; i++)
+	{
+		temp = maxright[i] + maxleft[i];
+	}
 }
